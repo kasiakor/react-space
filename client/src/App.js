7125e42d2a62c85d0e7dfc5ed840a373aef6335b
kasia.kordys <kasia.kordys@gmail.com>
2023-09-22 12:45:35 +0300

    add client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src={logo}
+        alt="spacex logo"
+        style={{ width: 300, display: "block", margin: "auto" }}
+      />
+    </div>
+  );
+}
+
+export default App;
