6895b6a5ef959cb12f41f78152a9ba23cf25a310
kasia.kordys <kasia.kordys@gmail.com>
2023-09-23 15:29:00 +0300

    add apollo dep

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import logo from "./logo.svg";
+import { InMemoryCache, ApolloClient } from "@apollo/client";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  link: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <img
-        src={logo}
-        alt="spacex logo"
-        style={{ width: 300, display: "block", margin: "auto" }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="spacex logo"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
